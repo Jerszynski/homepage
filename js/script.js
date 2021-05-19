0ff8fa4c9876ca3dd299a3958b84577bcce51140
Krzysztof Jerszyński <krzysztof.jerszynski@o2.pl>
2021-05-19 21:23:37 +0200

    Refactored Javascript

--- js/script.js
+++ js/script.js
@@ -1,50 +1,67 @@
-console.log("Witam!");
+{
+  const welcome = () => {
+    console.log(
+      "Hello my friend! Stay awhile and listen! And don't forget to Push the Button :) "
+    );
+  };
 
-let sectionButton = document.querySelector(".section__button");
-let bodyNewBGcolor = document.querySelector(".body");
+  const sectionButtonChanger = document.querySelector(".js__colorSwitch");
 
-sectionButton.addEventListener("click", () => {
-  sectionButton.innerText === "Zmień motyw"
-    ? (sectionButton.innerText = "Zmień motyw ponownie") &&
-      bodyNewBGcolor.classList.toggle("body__newBGcolor")
-    : (sectionButton.innerText = "Zmień motyw") &&
-      bodyNewBGcolor.classList.toggle("body__newBGcolor");
-});
+  const changeHeaderColorOnClick = () => {
+    const changer = document.querySelector(".js-coloredHeader");
 
-let sectionButtonChanger = document.querySelector(".js__colorSwitch");
-let changer = document.querySelector(".js-coloredHeader");
+    switch (sectionButtonChanger.innerText) {
+      case "Zmień kolor nagłówka na czerwony":
+        changer.classList.toggle("red__linksHeader");
+        sectionButtonChanger.innerText = "Zmień kolor nagłówka na niebieski";
+        break;
 
-sectionButtonChanger.addEventListener("click", () => {
-  switch (sectionButtonChanger.innerText) {
-    case "Zmień kolor nagłówka na czerwony":
-      changer.classList.toggle("red__linksHeader");
-      sectionButtonChanger.innerText = "Zmień kolor nagłówka na niebieski";
-      break;
+      case "Zmień kolor nagłówka na niebieski":
+        changer.classList.remove("red__linksHeader");
+        changer.classList.toggle("blue__linksHeader");
+        sectionButtonChanger.innerText = "Zmień kolor nagłówka na zielony";
+        break;
 
-    case "Zmień kolor nagłówka na niebieski":
-      changer.classList.remove("red__linksHeader");
-      changer.classList.toggle("blue__linksHeader");
-      sectionButtonChanger.innerText = "Zmień kolor nagłówka na zielony";
-      break;
+      case "Zmień kolor nagłówka na zielony":
+        changer.classList.remove("blue__linksHeader");
+        changer.classList.toggle("green__linksHeader");
+        sectionButtonChanger.innerText = "Zmień kolor nagłówka na żółty";
+        break;
 
-    case "Zmień kolor nagłówka na zielony":
-      changer.classList.remove("blue__linksHeader");
-      changer.classList.toggle("green__linksHeader");
-      sectionButtonChanger.innerText = "Zmień kolor nagłówka na żółty";
-      break;
+      case "Zmień kolor nagłówka na żółty":
+        changer.classList.remove("green__linksHeader");
+        changer.classList.toggle("yellow__linksHeader");
+        sectionButtonChanger.innerText = "Zmień kolor nagłówka na czarny";
+        break;
 
-    case "Zmień kolor nagłówka na żółty":
-      changer.classList.remove("green__linksHeader");
-      changer.classList.toggle("yellow__linksHeader");
-      sectionButtonChanger.innerText = "Zmień kolor nagłówka na czarny";
-      break;
+      case "Zmień kolor nagłówka na czarny":
+        changer.classList.remove("yellow__linksHeader");
+        sectionButtonChanger.innerText = "Zmień kolor nagłówka na czerwony";
+        break;
 
-    case "Zmień kolor nagłówka na czarny":
-      changer.classList.remove("yellow__linksHeader");
-      sectionButtonChanger.innerText = "Zmień kolor nagłówka na czerwony";
-      break;
+      default:
+        sectionButtonChanger.innerText = "Something were wrong!!";
+    }
+  };
 
-    default:
-      sectionButtonChanger.innerText = "Something were wrong!!";
-  }
-});
+  const sectionButton = document.querySelector(".section__button");
+
+  const changeBackgroundOnClick = () => {
+    const bodyNewBGcolor = document.querySelector(".body");
+
+    sectionButton.innerText === "Zmień motyw"
+      ? (sectionButton.innerText = "Zmień motyw ponownie") &&
+        bodyNewBGcolor.classList.toggle("body__newBGcolor")
+      : (sectionButton.innerText = "Zmień motyw") &&
+        bodyNewBGcolor.classList.toggle("body__newBGcolor");
+  };
+
+  const init = () => {
+    sectionButtonChanger.addEventListener("click", changeHeaderColorOnClick);
+
+    sectionButton.addEventListener("click", changeBackgroundOnClick);
+
+    welcome();
+  };
+  init();
+}
